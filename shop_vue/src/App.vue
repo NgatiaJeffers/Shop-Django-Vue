f7d31ccde0b7b0471c79f62b43cbff0d215f1dd5
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-07-07 01:59:06 +0300

    first commit

---
+++ shop_vue/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="app-wrapper">
+  <div class="app">
+    <Navigation />
+    <section class="section">
+      <router-view/>
+    </section>
+    <Footer />
+  </div>
+</div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: "app",
+  components: { Navigation, Footer },
+
+}
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+@import "../node_modules/bulma";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Quicksand", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+</style>
---
+++ shop_vue/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2021</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer {
+  height: 70%;
+  border-top: 1px solid #000;
+  padding: 1rem 1rem 1rem !important;
+}
+</style>
